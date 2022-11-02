96b579153e9acae1a4d23cfa80523cae35d36606
jeremy castillo <97201902+JeremyCastillo10@users.noreply.github.com>
2022-11-02 19:34:29 -0300

    dto

---
+++ app/src/main/java/com/ucne/parcial_jeremyy/data/dto/remote/VerboDto.kt
@@ -0,0 +1,8 @@
+package com.ucne.parcial_jeremyy.data.dto.remote
+
+data class VerboDto (
+    val verbo: String,
+    val Categoria: String,
+    val Nivel: String,
+    val Imagen: String
+        )
